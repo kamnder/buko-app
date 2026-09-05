3bcf727b7aec812e815f147ae5ee7f3b2ff87a7b
kamnder <120078325+kamnder@users.noreply.github.com>
2026-09-05 20:41:36 +0300

    test: avoid pumpAndSettle with infinite car animation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:buko/main.dart';
 void main() {
   testWidgets('BUKO opens on authentication screen', (tester) async {
     await tester.pumpWidget(const BukoApp());
-    await tester.pumpAndSettle();
+    await tester.pump(const Duration(milliseconds: 500));
     expect(find.text('BUKO'), findsOneWidget);
     expect(find.text('حبابك عشرة'), findsOneWidget);
     expect(find.text('رقم الهاتف السوداني'), findsOneWidget);
